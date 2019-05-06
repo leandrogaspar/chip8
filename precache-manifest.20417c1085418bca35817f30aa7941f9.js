dd2316d6ffb174e14a9cc0662c4ee4130f7ad76b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-06 23:56:29 +0000

    Deploy leandrogaspar/chip8 to github.com/leandrogaspar/chip8.git:gh-pages

---
+++ precache-manifest.20417c1085418bca35817f30aa7941f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692a7bc4dd6bda736a324c41c1e86b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eec681a357927901765b",
+    "url": "/static/css/main.281a3ef6.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "eec681a357927901765b",
+    "url": "/static/js/main.cb77add9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
